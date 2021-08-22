13991010a08c87d623bd921d7d4986c21c27db34
divcraft <t.ogonowski@wp.pl>
2021-08-22 13:13:53 +0200

    initial settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,5 +33,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: true,
+      },
+    },
+    // 'gatsby-plugin-layout',
+    'gatsby-plugin-root-import',
   ],
-}
+};
